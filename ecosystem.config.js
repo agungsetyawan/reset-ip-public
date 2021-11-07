6c519210bb146060d9ca12f68f4ea0844950751b
Agung Setyawan <agung.01setyawan@gmail.com>
2021-11-07 20:17:54 +0000

    change cron every 2 minutes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'reset-ip-public',
       script: './index.js',
-      cron_restart: '* * * * *',
+      cron_restart: '*/2 * * * *',
       autorestart: false,
       log_date_format: 'YYYY-MM-DD HH:mm:ss'
     }
